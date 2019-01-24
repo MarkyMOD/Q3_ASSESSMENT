ac6b7456dcffae6b037f8de68d87866cc8be8951
Mark Fantini <strictlybusinessemail242@gmail.com>
2019-01-24 12:56:30 -0700

    made all routes work, and am ready to deploy

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-
+import MessageList from './components/MessageList';
 
 class App extends Component {
 
@@ -13,7 +13,7 @@ class App extends Component {
     this.setState({ messages: json })
   }
 
-  postServer = async (posting) => {
+  postServer = async (posting, textField1, textField2, e) => {
     e.preventDefault()
     const response = await fetch(`https://assessmentme.herokuapp.com/messages/`, {
       method: 'POST',
@@ -25,11 +25,17 @@ class App extends Component {
     })
     const data = await response.json()
     this.setState({messages: [...this.state.messages, data]})
+
+    textField1.value = ""
+    textField2.value = ""
   }
 
-  patchServer = async (body, id) => {
+  patchServer = async (body, id, textField1, textField2, e) => {
     e.preventDefault()
-
+    if(id===0){
+      window.alert("Please Select a Message")
+      return
+    }
     const response = await fetch(`https://assessmentme.herokuapp.com/messages/${id}`, {
       method: 'PATCH',
       body: JSON.stringify(body),
@@ -43,9 +49,11 @@ class App extends Component {
     const index = map.indexOf(id)
     this.setState({messages: [...this.state.messages.slice(0, index), data, ...this.state.messages.slice(index + 1)]})
 
+    textField1.value = ""
+    textField2.value = ""
   }
 
-  deleteServer = async (id) => {
+  deleteServer = async (id, e) => {
     e.preventDefault()
     const response = await fetch(`https://assessmentme.herokuapp.com/messages/${id}`, {
       method: 'DELETE',
@@ -55,6 +63,7 @@ class App extends Component {
       }
     })
     const data = await response.json()
+    console.log("data", data)
     const clone = this.state.messages.filter(message => message.id !== id)
     this.setState({messages: [...clone]})
   }
@@ -66,7 +75,7 @@ class App extends Component {
           <MessageList messages={this.state.messages} postServer={this.postServer} deleteServer={this.deleteServer} patchServer={this.patchServer}/>
         </div>
       </div>
-    );
+    )
   }
 }
 
--- src/components/MessageList.js
+++ src/components/MessageList.js
@@ -25,15 +25,15 @@ class MessageList extends Component {
           {messageList}
         </div>
         <form className="form-inline my-2 my-lg-0">
-              <input className="form-control mr-sm-2" type="text" placeholder="Name" onKeyUp={e => this.setState({ update: {...this.state.update, name: e.target.value}, updateField1: e.target})}/>
-            <input className="form-control mr-sm-2" type="text" placeholder="Message" onKeyUp={e => this.setState({ update: {...this.state.update, message: e.target.value}, updateField2: e.target})}/>
+              <input className="form-control mr-sm-2" type="text" placeholder="Update Name" onKeyUp={e => this.setState({ update: {...this.state.update, name: e.target.value}, updateField1: e.target})}/>
+            <input className="form-control mr-sm-2" type="text" placeholder="Update Message" onKeyUp={e => this.setState({ update: {...this.state.update, message: e.target.value}, updateField2: e.target})}/>
           <button className="btn btn-outline-danger my-2 my-sm-0" type="submit" onClick={e => this.props.patchServer(this.state.update, this.state.id, this.state.updateField1, this.state.updateField2, e)}>Update Selected</button>
         </form>
 
         <form className="form-inline my-2 my-lg-0">
-              <input className="form-control mr-sm-2" type="text" placeholder="Name" onKeyUp={e => this.setState({ post: {...this.state.post, name: e.target.value}, postField1: e.target})}/>
-            <input className="form-control mr-sm-2" type="text" placeholder="Message" onKeyUp={e => this.setState({ post: {...this.state.post, message: e.target.value}, postField2: e.target})}/>
-          <button className="btn btn-outline-success my-2 my-sm-0" type="submit" onClick={e => this.props.postServer(this.state.post, this.state.postField1, this.state.postField2, e)}>Submit</button>
+              <input className="form-control mr-sm-2" type="text" placeholder="New Name" onKeyUp={e => this.setState({ post: {...this.state.post, name: e.target.value}, postField1: e.target})}/>
+            <input className="form-control mr-sm-2" type="text" placeholder="New Message" onKeyUp={e => this.setState({ post: {...this.state.post, message: e.target.value}, postField2: e.target})}/>
+          <button className="btn btn-outline-success my-2 my-sm-0" type="submit" onClick={e => this.props.postServer(this.state.post, this.state.postField1, this.state.postField2, e)}>Post</button>
         </form>
       </div>
     )
